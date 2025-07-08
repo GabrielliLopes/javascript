eaeaf0bf5a5e27504c7b87e5fc37f9f32fce2bfb
Gabrielli Lopes <l.lopesgabrielli@gmail.com>
2025-07-07 23:01:58 -0300

    Add files via upload

---
+++ javascript/exercícios/repetições/ambiente01.js
@@ -0,0 +1,12 @@
+/*var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <=10)
+ */
+
+console.log('Vai começar...')
+for(var c = 1; c <= 5; c++){
+    console.log(c)
+}
+console.log('FIM!')
