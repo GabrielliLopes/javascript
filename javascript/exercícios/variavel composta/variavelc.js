47a1cd8d49389346814933fa14d13639b695830b
Gabrielli Lopes <l.lopesgabrielli@gmail.com>
2025-07-08 21:28:22 -0300

    Add files via upload

---
+++ javascript/exercícios/variavel composta/variavelc.js
@@ -0,0 +1,7 @@
+let num = [5, 8, 2, 9, 3];
+num.sort(); // Ordena como strings por padrão (ex: 1, 2, 3, 5, 8, 9)
+num.push(1); // Adiciona o número 1 ao final do array
+
+console.log(num);
+console.log(`O vetor tem ${num.length} posições`);
+console.log(`O primeiro valor do vetor é ${num[0]}`);
